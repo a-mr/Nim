2abbecbeec2cd547a57069ba14c50ae1fc170fd6
Andrey Makarov <ph.makarov@gmail.com>
2020-02-05 01:47:47 +0300

    consistent handling of relative=true for wsOpenDir

--- lib/pure/os.nim
+++ lib/pure/os.nim
@@ -2140,6 +2140,7 @@ iterator tryWalkDir*(dir: string, relative=false): WalkStep {.
   ## 
   ## - ``kind=wsOpenDir`` is yielded once after opening, contains ``openStatus``
   ##                      of type ``OpenDirStatus``
+  ##                      (when `relative=true` the path is '.')
   ## - then it yields zero or more entries, each can be of the following kind:
   ##    - ``kind=wsEntryOk``: signifies normal entries with
   ##                          path component ``entryType``
@@ -2208,7 +2209,7 @@ iterator tryWalkDir*(dir: string, relative=false): WalkStep {.
         of ERROR_ACCESS_DENIED: odAccessDenied
         else: odUnknownError
       else: odOpenOk
-    step = sOpenDir(status, dir)
+    step = sOpenDir(status, if relative: "." else: dir)
 
     var firstStep = true
     while true:
@@ -2279,7 +2280,7 @@ iterator tryWalkDir*(dir: string, relative=false): WalkStep {.
           elif errno == EACCES: odAccessDenied
           else: odUnknownError
       else: odOpenOk
-    step = sOpenDir(status, dir)
+    step = sOpenDir(status, if relative: "." else: dir)
 
     while true:
       if not skip:
